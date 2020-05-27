aad63941024a32910487e4a2cc5230c00cdeb1c6
yaleksandrova21 <54073652+yaleksandrova21@users.noreply.github.com>
2020-05-27 14:08:16 +0300

    Revert "First"

--- arr.js
+++ arr.js
@@ -3,9 +3,7 @@ var n = 4, m = 4;
 var mas = [];
 for (var i = 0; i < m; i++){
 
-    mas[i] = []; 
+    mas[i] = [];
     for (var j = 0; j < n; j++){
         mas[i][j] = 0;
 }}
-
-console.log("ggggggggggggggggggg"); 
